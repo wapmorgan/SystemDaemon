b24a6a30516ab3f8963441da4b5d85ab54c98f3e
wapmorgan <wapmorgan@gmail.com>
2018-02-20 00:10:44 +0300

    Update handleConsole()

--- src/DaemonManager.php
+++ src/DaemonManager.php
@@ -5,6 +5,8 @@ use Exception;
 
 class DaemonManager
 {
+    public $restartTimeout = 30;
+
     /**
      * @var AbstractDaemon Daemon
      */
@@ -27,7 +29,8 @@ class DaemonManager
                     $pid = $this->daemon->start();
                     fwrite(STDOUT, 'Successfully started. Pid is '.$pid.PHP_EOL);
                 } catch (Exception $e) {
-                    fwrite(STDERR, 'Daemon can\'t be launched. Reason: '.$e->getMessage().PHP_EOL);
+                    fwrite(STDERR, 'Daemon can\'t be launched. Reason: '.$e->getMessage().' ('.$e->getCode().')'.PHP_EOL);
+                    fwrite(STDERR, $e->getTraceAsString().PHP_EOL);
                     exit(1);
                 }
                 break;
@@ -37,28 +40,55 @@ class DaemonManager
                 if ($status === false)
                     fwrite(STDOUT, 'Daemon is not running.'.PHP_EOL);
                 else {
-                    echo 'Daemon is running. Pid: '.$status->pid.PHP_EOL;
+                    fwrite(STDOUT, 'Daemon is running. Pid: '.$status->pid.PHP_EOL);
                 }
                 break;
 
             case 'stop':
+                $status = $this->daemon->getStatus();
+                if ($status === false) {
+                    fwrite(STDOUT, 'Daemon is not running'.PHP_EOL);
+                    break;
+                }
+
                 $result = $this->daemon->stop();
-                if ($result === true)
-                    echo 'Successfully stopped'.PHP_EOL;
-                else if ($result === false)
-                    echo 'Can\'t stop daemon.'.PHP_EOL;
+                if ($result === true) {
+                    $timeout = time() + $this->restartTimeout;
+                    while (time() < $timeout && $this->daemon->getStatus() !== false) {
+                        sleep(1);
+                    }
+
+                    if ($this->daemon->getStatus() === false) {
+                        fwrite(STDOUT, 'Successfully stopped' . PHP_EOL);
+                        return true;
+                    } else {
+                        fwrite(STDERR, 'Stopping is failed'.PHP_EOL);
+                        return false;
+                    }
+                } else if ($result === false) {
+                    fwrite(STDOUT, 'Can\'t stop daemon.' . PHP_EOL);
+                    return false;
+                }
+                break;
+
+            case 'restart':
+                $status = $this->daemon->getStatus();
+                if ($status !== false)
+                    $this->handleConsole(1, [__FILE__, 'stop']);
+
+                $this->handleConsole(1, [__FILE__, 'start']);
                 break;
 
             case 'kill':
                 $result = $this->daemon->kill();
                 if ($result === true)
-                    echo 'Successfully stopped'.PHP_EOL;
+                    fwrite(STDOUT, 'Successfully stopped'.PHP_EOL);
                 else if ($result === false)
-                    echo 'Can\'t stop daemon.'.PHP_EOL;
+                    fwrite(STDOUT, 'Can\'t stop daemon.'.PHP_EOL);
                 break;
 
             default:
-                echo 'Manager for daemon "'.$this->daemon->name.'". Available operations: '.$argv[0].' (start | status | stop | kill)'.PHP_EOL;
+                fwrite(STDOUT, 'Manager for daemon "'.$this->daemon->name.'". Available operations: '.$argv[0].' (start | status | stop | restart | kill)'.PHP_EOL);
                 break;
         }
     }
